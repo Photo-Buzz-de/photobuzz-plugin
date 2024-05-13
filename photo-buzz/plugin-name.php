6e214c3e0de77c5140def7edf0424b6aa02cdb06
m-kunz <martin@kunzmanns.com>
2024-05-13 16:21:12 +0200

    Update plugin-name.php

--- photo-buzz/plugin-name.php
+++ photo-buzz/plugin-name.php
@@ -8,20 +8,15 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
  * @since             1.0.0
  * @package           Plugin_Name
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Photo-Buzz Plugin
  * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Author:            Martin
  * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       photobuzz
  * Domain Path:       /languages
  */
 
